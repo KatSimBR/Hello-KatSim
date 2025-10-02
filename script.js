842b7c78e0417cdab8da06e1a492665ece608d48
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-10-02 08:42:58 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello KatSim</title>
+  </head>
+  <body>
+    <h1>Hello KatSim!</h1>
+    <script>
+      console.log("Hello KatSim!");
+    </script>
+  </body>
+</html>
